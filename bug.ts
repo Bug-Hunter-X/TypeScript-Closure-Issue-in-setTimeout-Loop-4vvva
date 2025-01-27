8194f594402d0ef1a375b391f672162448a4612c
BugHunter-X <askzenai@gmail.com>
2025-01-27 19:00:06 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,17 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5); // Works fine
+
+function printNumbers2(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 0);
+  }
+}
+
+printNumbers2(5); // Prints 6 five times instead of 1, 2, 3, 4, 5
